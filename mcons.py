8ad1a518c43bf310321b9ab0e7023b5c4272b9a8
w453908766 <w453908766@126.com>
2025-01-19 20:53:36 +0800

    implement dir

--- mcons.py
+++ mcons.py
@@ -1,62 +1,81 @@
 
-import os
+from os import path, stat, makedirs
 import subprocess
 from typing import List, Union
 import dask
 from dask import delayed
 
-def join_command(args: List[Union[str, List[str]]]) -> List[str]:
+build_dir = ""
+root_dir = ""
+
+def join_string_list(strlist):
   result = []
-  for item in args:
+  for item in strlist:
     if isinstance(item, str):
       result.extend(item.split())
     elif isinstance(item, List):
-      for s in item:
-        if isinstance(s, str):
-          result.extend(s.split()) 
-        else:
-          raise "type error"
+      result.extend(join_string_list(item))
     else:
       raise "type error"
   return result
 
-def run_command(*args: Union[str, List[str]]):
-  line = join_command(args)
-  print(' '.join(line))
-  result = subprocess.run(line, cwd = ".", capture_output=True)
+def run_command(cwd, *args: Union[str, List[str]]):
+  line = join_string_list(args)
+  print(cwd, ": ", ' '.join(line))
+  result = subprocess.run(line, cwd=cwd, capture_output=True)
   if result.stderr:
     print(result.stderr.decode('utf-8'))
   if result.stdout:
     print(result.stdout.decode('utf-8'))
   if result.returncode != 0:
-    raise f"error status code: 1"
+    raise f"error status code: {result.returncode}"
 
 def need_process(target: str, deps: List[str]):
-  if not os.path.exists(target): return True
+  if not path.exists(target): return True
 
-  target_stat = os.stat(target)
+  target_stat = stat(target)
   for dep in deps:
     if len(dep) == 0: continue
-    if not os.path.exists(dep):
+    if not path.exists(dep):
       raise f"error: file not found: {dep}"
-    dep_stat = os.stat(dep)
+    dep_stat = stat(dep)
     if target_stat.st_mtime < dep_stat.st_mtime:
       return True
   return False
 
-def to_delayed(x):
+def to_delayed(pydir, x): 
   if isinstance(x, str):
-    return delayed(x)
+    if path.isabs(x):
+      return delayed(x) 
+    else:
+      return delayed(path.join(pydir, x))
   else:
     return x
 
-@delayed
-def task(target, dependencies, func):
-  dependencies1 = map(to_delayed, dependencies)
-  deps = dask.compute(dependencies1)[0]
-  if need_process(target, deps):
-    func(deps)
-  return target
+def get_map_dir(pydir):
+  if root_dir != path.commonpath([root_dir, pydir]):
+    raise f"error: {pydir} is not in {root_dir}"
+  else:
+    rel = path.relpath(pydir, root_dir)
+    dir = path.join(build_dir, rel)
+    makedirs(dir, exist_ok=True)
+    return dir
 
-def run(t):
+@delayed
+def task(pyfile, target, dependencies, func):
+  pydir = path.dirname(path.abspath(pyfile))
+  dependencies1 = [to_delayed(pydir, x) for x in dependencies]
+  deps = dask.compute(dependencies1)[0]
+  map_dir = get_map_dir(pydir)
+
+  target1 = path.join(map_dir, target)
+  if need_process(target1, deps):
+    func(map_dir, deps)
+  return target1
+
+def run(root_pyfile, t):
+  global build_dir
+  global root_dir
+  build_dir = path.abspath(path.curdir)
+  root_dir = path.dirname(path.abspath(root_pyfile))
   return dask.compute(t)[0]
