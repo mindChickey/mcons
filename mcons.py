6563bddf645c23a29717ba17e829aa9d2001095d
w453908766 <w453908766@126.com>
2025-01-21 20:42:18 +0800

    update

--- mcons.py
+++ mcons.py
@@ -4,6 +4,11 @@ import subprocess
 import tempfile
 from core import check_depend, memo, need_update, cc, parse_depfile
 
+def run_command_output(cwd, line, outfile):
+  result = subprocess.run(line.split(), cwd=cwd, stdout=outfile)
+  if result.returncode != 0:
+    raise f"error status code: {result.returncode}"
+
 def run_command(cwd, line):
   print(cwd, ": ", line)
   result = subprocess.run(line.split(), cwd=cwd, capture_output=True)
@@ -25,23 +30,23 @@ def replace_extension(new_extension):
     return name + new_extension
   return f
 
-def cons_object(cm, src, compile_templ):
+def cons_object(cm, src, obj, compile_templ):
   def f():
     src1 = cm.src(src)
-    obj = cm.target(replace_extension(".o")(src))
-    if check_depend(cm, obj):
-      line = compile_templ.format(src1, obj)
+    target = cm.target(obj)
+    if check_depend(cm, target):
+      line = compile_templ.format(src1, target)
       run_command(cm.build_dir, line)
       with tempfile.NamedTemporaryFile(mode='w+') as mf:
         line1 = line + f" -MM -MF {mf.name}"
-        run_command(cm.build_dir, line1)
-        cc.depend_map[obj] = parse_depfile(mf)
-    return obj
+        run_command_output(cm.build_dir, line1, None)
+        cc.depend_map[target] = parse_depfile(mf)
+    return target
   return memo(f)
 
 def pack_ar(cm, name, sources, compile_templ):
   def f():
-    tasks = [cons_object(cm, src, compile_templ) for src in sources]
+    tasks = [cons_object(cm, src, replace_extension(".o")(src), compile_templ) for src in sources]
     objects = cc.compute(tasks)
     target = cm.target(name)
     if need_update(target, objects):
