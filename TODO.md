eaf84a40bd83586a013a98837e2babdb2bc083b3
w453908766 <w453908766@126.com>
2025-01-23 16:49:04 +0800

    extract object_need_update from cons_object

--- TODO.md
+++ TODO.md
@@ -1,11 +1,10 @@
 
 # 选项功能
 # 选项初始化
-# 生成 compile_commands.json
 # 选项线程数
 
-# 引申到其它生成类工具， latex 等
-
-# 脚本或dep文件修改
+# 脚本或选项修改
 # clean, install 和其它模式
 # watch 功能
+
+# 输出文件需要加版本号
--- src/object_rule.py
+++ src/object_rule.py
@@ -20,16 +20,27 @@ def update_depend(cm, target, line):
   except:
     exit(1)
 
-def update_and_compile_if_need(cm, target, target_mtime, line):
-  deps = update_depend(cm, target, line)
-  if compare_depends_mtime(target_mtime, deps):
-    run_command(cm, line)
-  return target
+def object_need_update(cm, target, line):
+  if not path.exists(target): 
+    update_depend(cm, target, line)
+    return True
 
-def update_and_compile(cm, target, line):
-  update_depend(cm, target, line)
-  run_command(cm, line)
-  return target
+  target_mtime = path.getmtime(target)
+
+  if header_depend.mtime < target_mtime: 
+    deps = update_depend(cm, target, line)
+    return compare_depends_mtime(target_mtime, deps)
+
+  deps = header_depend.get(target)
+  if deps:
+    if compare_depends_mtime(target_mtime, deps):
+      update_depend(cm, target, line)
+      return True
+    else:
+      return False
+  else:
+    deps = update_depend(cm, target, line)
+    return compare_depends_mtime(target_mtime, deps)
 
 def cons_object(cm, src, obj, compile_templ):
   src1 = cm.src(src)
@@ -37,18 +48,6 @@ def cons_object(cm, src, obj, compile_templ):
   line = compile_templ.format(src1, target)
   compile_commands.push(cm.build_dir, src1, line)
 
-  if not path.exists(target): 
-    return update_and_compile(cm, target, line)
-
-  target_mtime = path.getmtime(target)
-
-  if header_depend.mtime < target_mtime: 
-    return update_and_compile_if_need(cm, target, target_mtime, line)
-
-  deps = header_depend.get(target)
-  if deps:
-    if compare_depends_mtime(target_mtime, deps):
-      update_and_compile(cm, target, line)
-    return target
-  else:
-    return update_and_compile_if_need(cm, target, target_mtime, line)
+  if object_need_update(cm, target, line):
+    run_command(cm, line)
+  return target
