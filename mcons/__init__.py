bbecfc99cb10198e8fe2b63af9f43c67a3d47dd0
mindChickey <w453908766@126.com>
2025-10-29 21:46:50 +0800

    fix __init__.py

--- mcons/__init__.py
+++ mcons/__init__.py
@@ -1,25 +1,22 @@
 
-from .core.cons_module import ConsModule, Rule, TargetRule, SourceRule
-from .core.utils import memo, replace_ext, run_command
 from .core.env import batch, batch_map
+from .core.cons_module import ConsModule
+from .core.utils import memo, replace_ext, run_command
 
 from .rules.object_rule import object_rule
+from .rules.rule import Rule, TargetRule, SourceRule
 from .rules.task import pack_ar, task, cons_object_list, phony_target, rule
 
 from .modes.run_mode import run_cons
-from .modes.run_init import  reg_init_mode
-from .modes.run_clean import reg_clean_mode
-from .modes.run_build import reg_build_mode
-from .modes.run_watch import reg_watch_mode
 
 __all__ = [
-  "ConsModule", "Rule", "TargetRule", "SourceRule",
+  "batch", "batch_map",
+  "ConsModule",
+  "memo", "replace_ext", "run_command",
 
   "object_rule", 
+  "Rule", "TargetRule", "SourceRule",
   "pack_ar", "task", "cons_object_list", "phony_target", "rule",
-  "memo", "replace_ext", "run_command",
-  "batch", "batch_map",
 
-  "run_cons",
-  "reg_init_mode", "reg_clean_mode", "reg_build_mode", "reg_watch_mode"
+  "run_cons"
 ]
